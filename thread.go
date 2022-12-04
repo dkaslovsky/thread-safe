4291fb1cd721efe6fbf41c5b7346d60c3d749501
dkaslovsky <dkaslovsky@gmail.com>
2022-12-03 19:06:40 -0700

    thread header

--- thread.go
+++ thread.go
@@ -18,11 +18,25 @@ func (t *thread) len() int {
 }
 
 func (t *thread) String() string {
-	tweetStrs := []string{}
+	tweetStrs := []string{t.header()}
 	threadLen := t.len()
 	for i, tweet := range t.tweets() {
 		tweetStr := fmt.Sprintf("[%d/%d] %s", i+1, threadLen, tweet.Text)
 		tweetStrs = append(tweetStrs, tweetStr)
 	}
 	return strings.Join(tweetStrs, "\n---\n")
+}
+
+func (t *thread) header() string {
+	if t.len() == 0 {
+		return ""
+	}
+	first := t.tweets()[0]
+	headerStrs := []string{
+		fmt.Sprintf("URL: \t\t\t%s", first.URL),
+		fmt.Sprintf("Author Name: \t\t%s", first.AuthorName),
+		fmt.Sprintf("Author Handle: \t\t%s", first.AuthorHandle),
+		fmt.Sprintf("Conversation ID: \t%s", first.ConversationID),
+	}
+	return strings.Join(headerStrs, "\n")
 }
