dd5b2b3abbb15015356f1858cb8dbdafd11a517f
dkaslovsky <dkaslovsky@gmail.com>
2022-12-30 12:22:24 -0700

    cli root help text

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 // Run executes the top level command
 func Run(name string, version string, cliArgs []string) error {
 	if len(cliArgs) <= 1 {
-		// TODO: printUsage(name)
+		printUsage(name)
 		return nil
 	}
 
@@ -22,12 +22,32 @@ func Run(name string, version string, cliArgs []string) error {
 	case "render":
 		return render.Run(args)
 	case "--help", "-help", "-h":
-		// TODO: printUsage(name)
+		printUsage(name)
 		return nil
 	case "--version", "-version", "-v":
-		// TODO: printVersion(name, version)
+		printVersion(name, version)
 		return nil
 	default:
 		return fmt.Errorf("unknown command %s", cmd)
 	}
+}
+
+func printUsage(name string) {
+	fmt.Printf("%s saves a local copy of a Twitter thread\n", name)
+
+	fmt.Print("\nUsage:\n")
+	fmt.Printf("  %s [flags]\n", name)
+	fmt.Printf("  %s [command]\n", name)
+
+	fmt.Print("\nAvailable Commands:\n")
+	fmt.Print("  thread\tsaves thread content and generates a corresponding html file\n")
+	fmt.Print("  render\tregenerates an html file from a previously saved thread\n")
+
+	fmt.Print("\nFlags:\n")
+	fmt.Printf("  -h, -help\thelp for %s\n", name)
+	fmt.Printf("  -v, -version\tversion for %s\n", name)
+}
+
+func printVersion(name string, version string) {
+	fmt.Printf("%s v%s\n", name, version)
 }
