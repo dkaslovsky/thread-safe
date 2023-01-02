e7f6703f7ba032da16692f49e6ee358dc2b90b27
dkaslovsky <dkaslovsky@gmail.com>
2023-01-02 14:37:30 -0700

    read token from file

--- cmd/env/env.go
+++ cmd/env/env.go
@@ -1,15 +1,20 @@
 package env
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 const (
 	// Path is the name of the environment variable indicating path for saving threads
 	Path = "THREAD_SAFE_PATH"
-	// Token is the name of the environment variable indicating Twitter API bearer token
+	// Token is the name of the environment variable containing the Twitter API bearer token
 	Token = "THREAD_SAFE_TOKEN" // nolint:gosec
+	// tokenFileName is the name of the file in the user's $HOME directory containing the Twitter API bearer token
+	tokenFileName = ".thread-safe"
 )
 
 // Args holds environment variable values
@@ -18,7 +23,7 @@ type Args struct {
 	Token string
 }
 
-// Parse loads environment variables or sets defaults
+// Parse parses values from the environment
 func Parse() *Args {
 	path := "."
 	if p, ok := os.LookupEnv(Path); ok {
@@ -28,6 +33,8 @@ func Parse() *Args {
 	token := ""
 	if t, ok := os.LookupEnv(Token); ok {
 		token = t
+	} else {
+		token = readTokenFile()
 	}
 
 	return &Args{
@@ -39,6 +46,37 @@ func Parse() *Args {
 // Usage returns a string describing the environment variables
 func Usage() string {
 	return fmt.Sprintf(usage, Path, Token)
+}
+
+func readTokenFile() string {
+	tokenFilePath := filepath.Clean(filepath.Join(os.ExpandEnv("$HOME"), tokenFileName))
+
+	file, err := os.Open(tokenFilePath)
+	if err != nil {
+		return ""
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	var token string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineParts := strings.Split(line, "=")
+		if len(lineParts) != 2 {
+			continue
+		}
+		if strings.ToLower(strings.TrimSpace(lineParts[0])) == "token" {
+			token = strings.TrimSpace(lineParts[1])
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return ""
+	}
+
+	return token
 }
 
 var usage = `Environment Variables:
